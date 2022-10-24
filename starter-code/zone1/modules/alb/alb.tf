0307e49eac197ad09adb74754b96053e89987f61
Vyacheslav Khvan <vykhvan@gmail.com>
2022-10-24 11:13:17 +0600

    Updated alb module

--- starter-code/zone1/modules/alb/alb.tf
+++ starter-code/zone1/modules/alb/alb.tf
@@ -13,17 +13,19 @@ resource "aws_lb_target_group_attachment" "udacity" {
 }
 
 resource "aws_lb" "udacity" {
-  name                       = "udacity-lb-tf"
-  internal                   = false
-  load_balancer_type         = "application"
-  security_groups            = [var.ec2_sg]
-  subnets                    = var.subnet_id
+  name               = "udacity-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.ec2_sg]
+  subnets            = var.subnet_id
+
   enable_deletion_protection = false
+
 }
 
 resource "aws_lb_listener" "udacity" {
-  load_balancer_arn = aws_lb.udacity.arg
-  port              = 80
+  load_balancer_arn = aws_lb.udacity.arn
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
