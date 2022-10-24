72514086f4e30933f8ba93492dbefdffbb084731
Vyacheslav Khvan <vykhvan@gmail.com>
2022-10-24 11:13:58 +0600

    Updated terraform bucket

--- starter-code/zone1/_config.tf
+++ starter-code/zone1/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-vkhvan"
+     bucket = "udacity-terraform-vkhvan"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
